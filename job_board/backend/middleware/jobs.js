c42c3d426760104c30815f2f9665e2c71be6a342
AndressaSRodrigues <srandressa2@gmail.com>
2023-12-10 12:29:28 -0300

    Updated middleware to check isCompany and isPerson

--- job_board/backend/middleware/jobs.js
+++ job_board/backend/middleware/jobs.js
@@ -1,34 +1,39 @@
 const jwt = require('jsonwebtoken');
 const { secretKey } = require('../config');
 
-const isCompany = (req, res, next) => {
-    const token = req.header('Authorization');
+const checkUserRole = (allowedRole) => {
+    return (req, res, next) => {
+        const token = req.header('Authorization');
 
-    if (!token || !token.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Unauthorized: Invalid token format' });
-    }
-
-    const tokenValue = token.split(' ')[1];
-
-    try {
-        const decoded = jwt.verify(tokenValue, secretKey);
-
-        if (!decoded || !decoded.role) {
-            return res.status(403).json({ message: 'Forbidden: Role information not found in token' });
+        if (!token || !token.startsWith('Bearer ')) {
+            return res.status(401).json({ message: 'Unauthorized: Invalid token format.' });
         }
 
-        const userRole = decoded.role;
+        const tokenValue = token.split(' ')[1];
 
-        if (userRole === 'company') {
-            req.user = decoded;
-            next();
-        } else {
-            return res.status(403).json({ message: 'Forbidden: Unauthorized access.' });
+        try {
+            const decoded = jwt.verify(tokenValue, secretKey);
+
+            if (!decoded || !decoded.role) {
+                return res.status(403).json({ message: 'Forbidden: Role information not found in token.' });
+            }
+
+            const userRole = decoded.role;
+
+            if (userRole === allowedRole) {
+                req.user = decoded;
+                next();
+            } else {
+                return res.status(403).json({ message: 'Forbidden: Unauthorized access.' });
+            }
+        } catch (error) {
+            console.error('Token verification error:', error);
+            return res.status(401).json({ message: 'Unauthorized: Invalid token.' });
         }
-    } catch (error) {
-        console.error('Token verification error:', error);
-        return res.status(401).json({ message: 'Unauthorized: Invalid token' });
-    }
+    };
 };
 
-module.exports = { isCompany };
+const isCompany = checkUserRole('company');
+const isPerson = checkUserRole('person');
+
+module.exports = { isCompany, isPerson };
