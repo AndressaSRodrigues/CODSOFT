7f00551ea9786d099f433265bf76582ffcabb52c
AndressaSRodrigues <srandressa2@gmail.com>
2023-12-11 14:41:49 -0300

    Added controller to create a new job

--- job_board/backend/controllers/jobs.js
+++ job_board/backend/controllers/jobs.js
@@ -1,9 +1,34 @@
+const { Job, create } = require("../models/jobs");
+
 const getJobs = (req, res) => {
     res.send('List of jobs');
 };
 
-const createJob = (req, res) => {
-    res.send('Job created successfully');
+const createJob = async (req, res, next) => {
+    try {
+        const {
+            title,
+            company,
+            level,
+            description,
+            salary,
+            startDate
+        } = req.body;
+
+        const newJob = new Job({
+            title,
+            company,
+            level,
+            description,
+            salary,
+            startDate
+        });
+
+        await create(newJob);
+        return res.status(201).json({ message: 'Job successfully posted.' });
+    } catch (error) {
+        return res.status(500).json({ message: `${error}` });
+    }
 };
 
 module.exports = { getJobs, createJob };
