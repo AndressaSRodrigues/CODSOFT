591982552706f74f97dd55b7468715488f06c431
AndressaSRodrigues <srandressa2@gmail.com>
2023-12-18 15:33:34 -0300

    Added updateUser w/ isOwnUser middleware check

--- job_board/backend/controllers/users.js
+++ job_board/backend/controllers/users.js
@@ -1,6 +1,7 @@
-const { deleteByEmail } = require('../models/user');
+const bcrypt = require('bcrypt');
+const { deleteByEmail, updateByEmail } = require('../models/user');
 
-const deleteUser =  async (req, res) => {
+const deleteUser = async (req, res) => {
     const { email } = req.params;
 
     try {
@@ -12,11 +13,27 @@ const deleteUser =  async (req, res) => {
 
         return res.status(200).json({ message: 'User deleted successfully' });
     } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Internal server error' });
+        return res.status(500).json({ message: `${error}` });
+    }
+};
+
+const updateUser = async (req, res) => {
+    const { email } = req.params;
+    const values = req.body;
+
+    if (values.password) {
+        values.password = bcrypt.hashSync(values.password, 10);
+    }
+
+    try {
+        const updatedUser = await updateByEmail(email, values);
+        return res.status(200).json(updatedUser);
+    } catch (error) {
+        return res.status(500).json({ message: `${error}` });
     }
 };
 
 module.exports = {
-    deleteUser
+    deleteUser,
+    updateUser
 };
--- job_board/backend/routes/users.js
+++ job_board/backend/routes/users.js
@@ -1,6 +1,7 @@
-const { deleteUser } = require("../controllers/users");
+const { deleteUser, updateUser } = require("../controllers/users");
 const isOwnUser = require("../middleware/checkUserLoggedIn");
 
 module.exports = (app) => {
     app.delete('/user/:email', isOwnUser, deleteUser);
+    app.patch('/user/update/:email', isOwnUser, updateUser);
 };
