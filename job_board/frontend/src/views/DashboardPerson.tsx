946f88814490ce58935df07e0ce18a79301c9165
AndressaSRodrigues <srandressa2@gmail.com>
2023-12-20 17:05:18 -0300

    Added person dashboard

--- job_board/frontend/src/views/DashboardPerson.tsx
+++ job_board/frontend/src/views/DashboardPerson.tsx
@@ -1,6 +1,23 @@
+import Navbar from "../components/Shared/Navbar"
+import SendIcon from '@mui/icons-material/Send'
+import BookmarkAddedIcon from '@mui/icons-material/BookmarkAdded'
+
 function DashboardPerson() {
+
+  const titleStyle = "text-primary text-2xl font-bold mb-6";
+
   return (
-    <div>DashboardPerson</div>
+    <>
+      <Navbar />
+      <div className="flex flex-col items-start justify-around m-12 gap-4 lg:flex lg:flex-row lg:items-start lg:m-16 lg:mx-52">
+        <div>
+          <h1 className={titleStyle}>Applications Sent <SendIcon /></h1>
+        </div>
+        <div className="mt-8 lg:ml-32 lg:mt-0">
+          <h1 className={titleStyle}>Saved Jobs <BookmarkAddedIcon /></h1>
+        </div>
+      </div>
+    </>
   )
 }
 
