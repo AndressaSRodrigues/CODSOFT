e27c87867965d13eab589215b6e24c66402e239d
AndressaSRodrigues <srandressa2@gmail.com>
2023-12-29 16:04:09 -0300

    React and Tailwind set up

---
+++ quiz_maker/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  
+  return (
+    <>
+      <h1 className="text-primary font-bold">QuizHub</h1>
+      <p className="text-second font-bold">Hello hello hello</p>
+    </>
+  )
+}
+
+export default App
---
+++ quiz_maker/frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#99CCCC',
+        'second': '#CC0066'
+      }
+    },
+  },
+  plugins: [],
+}
+
